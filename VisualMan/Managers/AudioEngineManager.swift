3ba1ab692c46c2513e8850509c373f27b82e0145
aabagdi <70984836+aabagdi@users.noreply.github.com>
2026-03-17 02:04:03 -0700

    fixed SwiftLint file length warning

--- VisualMan/Managers/AudioEngineManager.swift
+++ VisualMan/Managers/AudioEngineManager.swift
@@ -386,30 +386,3 @@ extension AudioEngineManager {
     }
   }
 }
-
-@MainActor
-private final class DisplayLinkStream: NSObject {
-  private var displayLink: CADisplayLink?
-  private var continuation: AsyncStream<Void>.Continuation?
-  
-  var frames: AsyncStream<Void> {
-    let (stream, continuation) = AsyncStream<Void>.makeStream(bufferingPolicy: .bufferingNewest(1))
-    self.continuation = continuation
-    let link = CADisplayLink(target: self, selector: #selector(onFrame))
-    link.preferredFrameRateRange = CAFrameRateRange.init(minimum: 60, maximum: 120, preferred: 120)
-    link.add(to: .current, forMode: .common)
-    self.displayLink = link
-    return stream
-  }
-  
-  func stop() {
-    displayLink?.invalidate()
-    displayLink = nil
-    continuation?.finish()
-    continuation = nil
-  }
-  
-  @objc private func onFrame() {
-    continuation?.yield()
-  }
-}
