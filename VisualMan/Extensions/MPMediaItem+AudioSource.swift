1c0a1bd58556b59619e108d94f020f5c84cfae33
aabagdi <70984836+aabagdi@users.noreply.github.com>
2026-03-17 13:43:25 -0700

    added cost limits to image caches

--- VisualMan/Extensions/MPMediaItem+AudioSource.swift
+++ VisualMan/Extensions/MPMediaItem+AudioSource.swift
@@ -11,10 +11,16 @@ import MediaPlayer
 private let thumbnailCache: NSCache<NSNumber, UIImage> = {
   let c = NSCache<NSNumber, UIImage>()
   c.countLimit = 100
+  c.totalCostLimit = 10 * 1024 * 1024
   return c
 }()
 
-private let fullArtworkCache = NSCache<NSNumber, UIImage>()
+private let fullArtworkCache: NSCache<NSNumber, UIImage> = {
+  let c = NSCache<NSNumber, UIImage>()
+  c.countLimit = 50
+  c.totalCostLimit = 50 * 1024 * 1024
+  return c
+}()
 
 extension MPMediaItem: AudioSource {
   var albumArt: UIImage? {
@@ -24,7 +30,8 @@ extension MPMediaItem: AudioSource {
     
     guard let albumArt = artwork?.image(at: CGSize(width: 512, height: 512)) else { return nil }
     
-    fullArtworkCache.setObject(albumArt, forKey: key)
+    let cost = Int(albumArt.size.width * albumArt.size.height * 4)
+    fullArtworkCache.setObject(albumArt, forKey: key, cost: cost)
     
     return albumArt
   }
@@ -36,7 +43,8 @@ extension MPMediaItem: AudioSource {
     
     guard let thumbnail = artwork?.image(at: CGSize(width: 60, height: 60)) else { return nil }
     
-    thumbnailCache.setObject(thumbnail, forKey: key)
+    let cost = Int(thumbnail.size.width * thumbnail.size.height * 4)
+    thumbnailCache.setObject(thumbnail, forKey: key, cost: cost)
     
     return thumbnail
   }
